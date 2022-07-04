b3eccab45430e5692ff090401f2768f6e407c526
MINSK\Siarhei_Vaitehovich <Siarhei_Vaitehovich@epam.com>
2022-07-05 00:12:02 +0300

    Add deploy script

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
       </main>
       <div className="footer" role="contentinfo">
         <a className="git" href="https://github.com/SW999/brainless-frontend" rel="nofollow" title="Github page">
-          <img src="../public/static/img/github.svg" alt="github" width="24" height="24"/>
+          <img src="./static/img/github.svg" width="24" height="24" alt="Github logo" />
         </a>Serj Vaitehovich<span className="divider"/>v. 1.0.0<span className="divider"/><a
         href="mailto:sergey7w@gmail.com?subject=Brainless-frontend" target="_blank" rel="noopener noreferrer"
         title="Email me"><span

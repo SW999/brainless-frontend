55911848712ec8489bebb68b576d4d77cdf6b494
MINSK\Siarhei_Vaitehovich <Siarhei_Vaitehovich@epam.com>
2022-07-06 15:28:19 +0300

    Add correct proxy to 3001 port; update content

--- client/src/utils/upload-data.js
+++ client/src/utils/upload-data.js
@@ -1,6 +1,6 @@
 export const uploadData = async (data, name) => {
   const json = JSON.stringify(data);
-  const blob = new Blob([json], { type: 'application/json' });
+  const blob = new Blob([json], {type: 'application/json'});
 
   const formData = new FormData();
   formData.append('myFile', blob, `${name}.json`);

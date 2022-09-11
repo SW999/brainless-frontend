ddda1c03f62d8c6f50ba926c772ed37f071b2ead
MINSK\Siarhei_Vaitehovich <Siarhei_Vaitehovich@epam.com>
2022-09-11 21:55:15 +0300

    Fix an image styles

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div className="footer" role="contentinfo">
         <a className="git" href="https://github.com/SW999/brainless-frontend" rel="nofollow" title="Github page">
           <img src="./static/img/github.svg" width="24" height="24" alt="Github logo"/>
-        </a>Serj Vaitehovich<span className="divider"/>v. 1.1.0<span className="divider"/><a
+        </a>Sergey Vaitehovich<span className="divider"/>v. 1.1.0<span className="divider"/><a
         href="mailto:sergey7w@gmail.com?subject=Brainless-frontend" target="_blank" rel="noopener noreferrer"
         title="Email me"><span
         role="img" aria-label="email me">📧</span></a>
